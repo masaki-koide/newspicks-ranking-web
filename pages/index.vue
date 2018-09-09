5ef9d5053b020c90c6363fd48f7ec294ccaa4d98
masaki.koide <ikasama0324@gmail.com>
2018-09-09 19:28:39 +0900

    feat: 初期表示を日間ランキングに修正

--- pages/index.vue
+++ pages/index.vue
@@ -9,12 +9,12 @@
         show-arrows
         fixed-tabs
         slider-color="white">
-        <v-tab @click="getRanking(null)">全体</v-tab>
-        <v-tab @click="getRanking('day')">24時間以内</v-tab>
-        <v-tab @click="getRanking('week')">1週間以内</v-tab>
-        <v-tab @click="getRanking('month')">1ヶ月以内</v-tab>
-        <v-tab @click="getRanking('half-year')">半年以内</v-tab>
-        <v-tab @click="getRanking('year')">1年以内</v-tab>
+        <v-tab @click="getRanking('day')">日間</v-tab>
+        <v-tab @click="getRanking('week')">週間</v-tab>
+        <v-tab @click="getRanking('month')">月間</v-tab>
+        <v-tab @click="getRanking('half-year')">半年間</v-tab>
+        <v-tab @click="getRanking('year')">年間</v-tab>
+        <v-tab @click="getRanking(null)">全期間</v-tab>
       </v-tabs>
       <div v-if="true">
         <v-card v-for="post in ranking" :key="post.url">
@@ -43,7 +43,7 @@ import AppLogo from '~/components/AppLogo.vue'
 
 export default {
   async asyncData({ store }) {
-    await store.dispatch('getRanking', null)
+    await store.dispatch('getRanking', 'day')
   },
   computed: {
     ...mapGetters(['ranking'])
