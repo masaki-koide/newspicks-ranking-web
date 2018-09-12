a26b5e85d0030d81e121a1f9189fdd81ee6c76e6
masaki.koide <ikasama0324@gmail.com>
2018-09-12 21:17:03 +0900

    Revert "test: 日間ランキングだけは再読込みするよう試してみる"
    This reverts commit ab5f6eedecb22679684cd3c0c0579899b8f6282d.

--- store/index.js
+++ store/index.js
@@ -28,7 +28,7 @@ const store = () => new Vuex.Store({
     async getRanking({ state, commit }, range) {
       try {
         const rankingKey = range ? range : 'default'
-        if (range !== 'day' && state.rankingObj[rankingKey]) {
+        if (state.rankingObj[rankingKey]) {
           commit('setCurrentRankingKey', rankingKey)
           return
         }
